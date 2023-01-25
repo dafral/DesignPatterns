48421663d8cee39ce3375a350e27dc97e07cd949
dafral <dafral1996@gmail.com>
2023-01-25 12:07:50 +0100

    Projectile prefab created

---
+++ Assets/Scripts/Ships/Weapons/Projectile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ships.Weapons
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+    
+    }
+}
