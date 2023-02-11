a1646768b0bec58043d64fd4c0b6e5fa6e3a09aa
dafral <dafral1996@gmail.com>
2023-02-11 14:45:16 +0100

    CompositeCommand created

---
+++ Assets/Scripts/Core/Commands/CompositeCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Commands
+{
+    public class CompositeCommand : ICommand
+    {
+        private readonly List<ICommand> _commands;
+
+        public CompositeCommand()
+        {
+            _commands = new List<ICommand>();
+        }
+
+        public void AddCommand(ICommand command)
+        {
+            _commands.Add(command);
+        }
+
+        public async Task Execute()
+        {
+            var tasks = new List<Task>(_commands.Count);
+            foreach (var command in _commands)
+            {
+                var task = command.Execute();
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
